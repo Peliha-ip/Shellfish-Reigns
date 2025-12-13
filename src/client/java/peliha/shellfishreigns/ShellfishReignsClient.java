a586316711ed9fff403b64f85b5da7303f2b66bb
Peliha <Philippevanasse@gmail.com>
2025-12-12 22:01:47 -0500

    Updated first commit of Shellfish Reigns.

---
+++ src/client/java/peliha/shellfishreigns/ShellfishReignsClient.java
@@ -0,0 +1,10 @@
+package peliha.shellfishreigns;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class ShellfishReignsClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
